d45ac917637422bcdb8cd029d5d1799015d074a7
daniel <daniel@192.168.2.103>
2014-04-08 19:23:27 +0200

    First Commit: Testgetrieben Entwicklung
    Es soll ein Spielemodell erstell werden
    Spiel: Spheres (siehe github)

---
+++ Model/src/GameModelTest.java
@@ -0,0 +1,10 @@
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+
+public class GameModelTest {
+
+	@Test
+	public void test() {
+		GameModel mockedGameModel = mock(GameModel.class);
+	}
+}
