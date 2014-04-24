eb1f7621710cd11f85c809baf32a0c882f76eada
daniel <daniel@192.168.2.103>
2014-04-24 22:30:35 +0200

    feinheiten und kommentare

--- Model/src/GameModelTest.java
+++ Model/src/GameModelTest.java
@@ -8,26 +8,28 @@ import org.junit.Test;
 public class GameModelTest {
 
 	public GameModel gModel;
-	public User mUser = mock(User.class);
+	public User mockedUser = mock(User.class);
 	
 	@Before
 	public void setUp(){
-		gModel = new GameModel(mUser);
+		gModel = new GameModel(mockedUser);
 	}
 	
 	@Test
 	public void checkGameMode(){		
 		//Beim ersten test wird 0 erwartet
-		when(mUser.getGameMode()).thenReturn(0);
+		when(mockedUser.getGameMode()).thenReturn(0);
 		assertEquals(0, gModel.getGameMode());
 		
-		when(mUser.getGameMode()).thenReturn(1);
+		//Hier wird der 2. und letzte erlaubte Wert 1 erwartet
+		when(mockedUser.getGameMode()).thenReturn(1);
 		assertEquals(1, gModel.getGameMode());
-	
-		when(mUser.getGameMode()).thenReturn(3);
+		
+		//Bei ungültiger Rückgabe vom User wird der Standardmodus 0 gewählt
+		when(mockedUser.getGameMode()).thenReturn(3);
 		assertEquals(0, gModel.getGameMode());
 		
-		when(mUser.getGameMode()).thenReturn(-3);
+		when(mockedUser.getGameMode()).thenReturn(-3);
 		assertEquals(0, gModel.getGameMode());
 	}
 }
